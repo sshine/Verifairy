0ca0ba6927e4b7eff8b8c4d5c5e4326acceeb81b
Simon Shine <shreddedglory@gmail.com>
2021-08-17 22:48:05 +0200

    import Test.Hspec for backwards compatibility on GHC 8.8.4

--- test/ParserTest.hs
+++ test/ParserTest.hs
@@ -14,6 +14,7 @@ import Data.Text.Read (hexadecimal)
 import Data.Void
 
 import Hedgehog
+import Test.Hspec
 import Test.Hspec.Megaparsec
 import Test.Tasty.Hspec
 --import Text.Megaparsec (parse, ParseErrorBundle)
