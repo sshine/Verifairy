7b20434063f93ca9ca914c03e5913e9a92a881f8
cfcs <you@example.com>
2022-05-19 03:00:42 +0200

    nicer svg output, add -i to control exit status

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,11 @@ module Main where
 import Control.Monad (unless)
 import Data.Foldable (for_, traverse_)
 import Data.Function ((&))
+import Data.Set (Set)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import qualified Data.Text.Lazy as Text.Lazy
 import Error.Diagnose (addFile, printDiagnostic)
 import Error.Diagnose.Compat.Megaparsec (errorDiagnosticFromBundle)
 import Options.Applicative
@@ -23,13 +25,14 @@ import Data.GraphViz
 import Data.Graph.Inductive.PatriciaTree
 import Data.Graph.Inductive.Graph (OrdGr(..) , Node)
 import Data.GraphViz.Printing
-import Data.GraphViz.Attributes.Complete(Justification(JLeft),Attribute(..))
+import Data.GraphViz.Attributes.Complete(Justification(JLeft),Attribute(..), EdgeType(..), Overlap(..))
 
 data Args = Args
   { srcFile :: FilePath,
     simplify :: String,
     dotFile :: String,
-    verbose :: Bool
+    verbose :: Bool,
+    ignore_constraints :: Bool
   }
   deriving (Show)
 
@@ -37,7 +40,11 @@ main :: IO ()
 main = runArgsParser >>= argsHandler
 
 argsHandler :: Args -> IO ()
-argsHandler Args {srcFile = srcFile, simplify=simplify, dotFile=dotFile, verbose = verbose} = do
+argsHandler Args {srcFile = srcFile
+                 , simplify=simplify
+                 , dotFile=dotFile
+                 , ignore_constraints = ignore_constraints
+                 , verbose = verbose} = do
   srcText <- Text.readFile srcFile
   case parseModel srcText of
     Left bundle ->
@@ -52,6 +59,11 @@ argsHandler Args {srcFile = srcFile, simplify=simplify, dotFile=dotFile, verbose
       Text.hPutStrLn stderr ("Processing file " <> Text.pack srcFile)
       let ms = VerifPal.Check.process model
       --
+      unless (not verbose) $
+        do putStrLn ""
+           putDoc (prettifyModelState ms)
+           putStrLn ""
+      --
       case simplify of
         "" -> pure ()
         simplified ->
@@ -65,11 +77,6 @@ argsHandler Args {srcFile = srcFile, simplify=simplify, dotFile=dotFile, verbose
              putStrLn ""
              putDoc (prettifyCanonExpr (simplifyExpr c_expr))
              putStrLn ""
-      --
-      unless (not verbose) $
-        do putStrLn ""
-           putDoc (prettifyModelState ms)
-           putStrLn ""
       --
       --putStrLn ""
       --putDoc (prettifyConfidentialityGraph (msConfidentialityGraph ms))
@@ -88,28 +95,47 @@ argsHandler Args {srcFile = srcFile, simplify=simplify, dotFile=dotFile, verbose
         putStrLn ""
       -- Bail out if we have errors or failing queries:
       unless (null (msErrors ms)) exitFailure
-      unless (all snd (msQueryResults ms)) exitFailure
+      unless (ignore_constraints || all snd (msQueryResults ms)) exitFailure
 
-mytoDot :: Show a => Show b => Data.Graph.Inductive.Graph.OrdGr Data.Graph.Inductive.PatriciaTree.Gr a b -> Text
+mytoDot :: Show a => Show b => Data.Graph.Inductive.Graph.OrdGr Data.Graph.Inductive.PatriciaTree.Gr a (Set (Set b)) -> Text
 mytoDot g =
+  -- TODO it would be nice to strip back arrows before emitting this graph
   -- nodeAttrs :: (Node, Link) -> Attributes
-  let edgeAttrs (x) =
+  -- https://graphviz.org/docs/attrs/stylesheet/ url to a stylesheet
+  -- https://graphviz.org/docs/attrs/tooltip/
+  -- https://graphviz.org/docs/attrs/xlabel/
+  -- https://graphviz.org/docs/outputs/vrml/ protocol analysis in 3D!
+  -- Root True on "attacker" node
+  let fixAlign :: Text -> Text
+      fixAlign text =
+        -- \l left-justifies the current line (as opposed to \n which centers)
+        Text.replace "\n" "\\l" . flip Text.append "\\l" $ text
+      ppEdge (_from,_to,s) =
+        let treated = if all null s then "" else show $ ppDoc s
+        in fixAlign $ Text.pack treated
+      edgeAttrs x@(from,to,_label) =
         [
-          toLabel . Text.pack . show $ ppDoc $ x
+          toLabel $ ppEdge x
+        , colors !! (from+to)
+        , Decorate True
         ]
       nodeAttrs (a,label) = -- fmtNode= \(_,label)-> [Label (StrLabel (L.pack label))]
-        [ toLabel . Text.pack . show $ ppDoc $ label
-        , shape BoxShape
-        , LabelJust JLeft
-        , colors !! a
+        [ toLabel . fixAlign . Text.pack . show $ ppDoc $ label
+        --, LabelTarget "a"
+        --, LabelURL "b"
+        --, LabelTooltip "foo"
+        , Tooltip $ Text.Lazy.fromStrict $Text.pack $ show a
+        , URL "javascript:alert(3)"
+        --, colors !! a
+        , Data.GraphViz.style filled
         --, bgColor [colors !! a]
         --fillColor [colors !! a]
         --, Style [SItem filled []]
         ]
-      colors = cycle $ map bgColor [ LightBlue
+      colors = cycle $ map color [ LightBlue
                                    , Red
                                    , Orange
-                                   , Yellow
+                                   --, Yellow
                                    , Green
                                    , Blue
                                    , Purple
@@ -124,8 +150,26 @@ mytoDot g =
                          fmtNode = nodeAttrs
                          , fmtEdge = edgeAttrs
                          , globalAttributes = [
+                             EdgeAttrs [
+                                 FontName "Mono"
+                                 , color Red --(RGB 0xfe 0x4a 0x49)
+                                 --, Constraint False
+                                 -- TODO if we use records we can have "ports" so
+                                 -- we can draw lines accurately from each argument
+                                 -- https://hackage.haskell.org/package/graphviz-2999.18.1.2/docs/src/Data-GraphViz-Attributes-Values.html#RecordField
+                                       ],
+                             GraphAttrs [
+                                 Splines Ortho
+--  data EdgeType
+--  = SplineEdges | LineEdges | NoEdges | PolyLine | Ortho | Curved | CompoundEdge
+                                 , Overlap $ PrismOverlap Nothing
+                                        ],
                              NodeAttrs [
                                  shape BoxShape
+                                 , NoJustify True
+                                 , shape BoxShape
+                                 , Group "hello"
+                                 , FontName "Mono"
                                  , LabelJust JLeft -- not JCenter
                                        ]
                                             ]
@@ -146,11 +190,22 @@ argsParserInfo =
     ]
 
 argsParser :: Parser Args
-argsParser = Args <$> srcFileParser <*> simplify <*> dotFile <*>verbose
+argsParser = Args <$> srcFileParser <*> simplify <*> dotFile <*> verbose <*> ignore_constraints
   where
-    verbose = flag False True . mconcat $ [ short 'v' ]
-    simplify = option str (long "simplify-constant" <> short 's' <> value "")
-    dotFile = option str (long "dot-file" <> value "")
+    verbose = flag False True . mconcat $ [
+      short 'v' <> help "be (extremely) verbose"
+      ]
+    ignore_constraints = flag False True . mconcat $ [
+      short 'i' <>
+      help "exit code 0 even with failed constraints. useful with --dot-file"
+      ]
+    simplify = option str (
+      long "simplify-constant" <> short 's' <> value "" <>
+        help "shows the named constant in minimal equivalent form" <>
+        metavar "CONSTANT"
+                          )
+    dotFile = option str (long "dot-file" <> value "" <>
+                          help "outputs a 'dot'/graphviz file: '--dot-file x.dot ; dot x.dot -Tsvg -o x.svg'")
     srcFileParser =
       strArgument . mconcat $
         [ metavar "<FILE.vp>"
